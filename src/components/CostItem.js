0d44c203d8158ace55ac246a3a79a7aaf6da0de0
Your Name <you@example.com>
2023-02-07 22:46:23 +0300

    CostItemStyle

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,6 +1,14 @@
+import "./CostItem.css";
+
 function CostItem () {
     return (
-        <div>Элемент расхода</div>
+        <div className='cost-item'>
+            <div>Дата</div>
+            <div className='cost-item__description'>
+                <h2>Холодильник</h2>
+                <div className='cost-item__price'>Цена</div>
+            </div>
+        </div>
     );
 }
 
